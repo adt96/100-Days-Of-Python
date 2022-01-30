c5997b54f95677c916f04eaacabd5f4720fbafd6
Aditya Shiva <35380459+adt96@users.noreply.github.com>
2022-01-30 17:03:27 +0530

    Add files via upload

---
+++ printing.py
@@ -0,0 +1,9 @@
+
+print("Hello World")
+print('Hello World')
+print("""Hello World""")
+print("hi ('Hello World')")
+print("Hello World\n Hello World")
+print("Hello"+" Aditya")
+print("Hello"+input("What is ur name ?"))
+print(len(input("Enter name")))
